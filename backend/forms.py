9cafe34e4cf02ddfa5969c627e9ef8ec82f04883
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-09 11:25:25 +0530

    User Register Logic added

--- backend/forms.py
+++ backend/forms.py
@@ -1,10 +1,10 @@
 from backend.models import CustomUser
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('email',)
+        fields = ('email', 'first_name', 'last_name', 'gender', 'dob', 'phone_no', )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,10 @@
+# frontend/urls.py
 from django.urls import path
-from frontend.views import home, qr_code_view
-from . import views
+from frontend.views import home, qr_code_view, webcam_feed, register
 
 urlpatterns = [
     path('', home, name='home'),
     path('qr/', qr_code_view, name='qr-code'),
-    path('webcam/',views.webcam_feed, name='webcam-feed')
+    path('webcam/', webcam_feed, name='webcam-feed'),
+    path('register', register, name='register')
 ]
