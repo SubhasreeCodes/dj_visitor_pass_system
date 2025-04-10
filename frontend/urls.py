bc7de84a0337f1aa169daef70af3f9282a0bfeef
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-10 10:32:29 +0530

    Capture And Send QR

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,16 @@
-# frontend/urls.py
+# urls.py
 from django.urls import path
-from frontend.views import home, qr_code_view, webcam_feed, register
+from frontend.views import home, qr_code_view, webcam_feed, register, decode_qr_from_image
 
 urlpatterns = [
     path('', home, name='home'),
-    path('qr/', qr_code_view, name='qr-code'),
+    path('/', home, name='home'),
+    path('/home', home, name='home'),
+
+    path('webcam/', webcam_feed, name='webcam-feed'),
+    path('qr-code/', qr_code_view, name='qr-code'),
+    path('decode-qr/', decode_qr_from_image, name='decode-qr'),
+
     path('webcam/', webcam_feed, name='webcam-feed'),
     path('register', register, name='register')
 ]
